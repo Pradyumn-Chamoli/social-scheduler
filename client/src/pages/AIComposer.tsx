5e26fafad11930528530da8e69e400b647495b45
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-07-06 01:13:49 +0530

    Added Sidebar menu

--- client/src/pages/AIComposer.tsx
+++ client/src/pages/AIComposer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AIComposer = () => {
   return (
     <div>
-      
+      AI Composer
     </div>
   )
 }
--- client/src/pages/Accounts.tsx
+++ client/src/pages/Accounts.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Accounts = () => {
   return (
     <div>
-      
+      Accounts
     </div>
   )
 }
