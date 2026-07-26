fac328fdbeb5d0664c00636431bd3d1cc115f95e
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-07-26 20:37:39 +0530

    Setted up database

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express, { Request, Response } from 'express';
+import cors from "cors";
+import { connect } from "node:http2";
+import connectDB from "./config/db.js";
+
+const app = express();
+
+//Database connection
+await connectDB();
+
+
+// Middleware
+app.use(cors())
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Server is Live!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
