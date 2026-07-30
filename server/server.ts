eadf88ea1a8ca5b82285538b8ae05f8e2d93b59d
Pradyumn Chamoli <pradyumnchamoli18@gmail.com>
2026-07-31 01:44:35 +0530

    made controllers to get,post and delete accounts

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import { connect } from "node:http2";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import socialAuthRouter from "./routes/socialAuthRoutes.js";
+import accountRouter from "./routes/accountRoutes.js";
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.get('/', (_req: Request, res: Response) => {
 
 app.use("/api/auth" , authRouter);
 app.use("/api/oauth" , socialAuthRouter);
+app.use("/api/accounts" , accountRouter);
 
 
 //Global error handler
